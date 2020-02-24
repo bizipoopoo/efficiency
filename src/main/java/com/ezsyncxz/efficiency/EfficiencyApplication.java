d62ed75c86f33a437e32133e9531c50b2a24da4e
chenwj <icebearbearbearbear@gmail.com>
2020-02-24 12:00:52 +0800

    初次提交

---
+++ src/main/java/com/ezsyncxz/efficiency/EfficiencyApplication.java
@@ -0,0 +1,13 @@
+package com.ezsyncxz.efficiency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfficiencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfficiencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ezsyncxz/efficiency/EfficiencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezsyncxz.efficiency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfficiencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
